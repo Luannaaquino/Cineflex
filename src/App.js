6e67347708da47c9ff259d273a2621b894b67903
Luanna Aquino <luannaluleticia@gmail.com>
2021-05-14 18:58:20 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import { useState } from "react";
 import Header from "./components/header"
 import Home from "./components/home"
 import MovieTime from "./components/movieTime"
+import BuyTicket from "./components/buyTicket"
+import Finish from "./components/finish"
 
 export default function App() {
-   
+    const [inputName, setInputName] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [successData, setSuccessData] = useState({});
+    const peopleData = {
+      name: inputName,
+      cpf: cpf,
+      ...successData,
+    };
+
+
     return (
     <BrowserRouter>
         <Header />
@@ -17,6 +28,23 @@ export default function App() {
             </Route>
             <Route path="/film/:filmId" exact>
                 <MovieTime />
+            </Route>
+            <Route path="/session/:sessionId" exact>
+                <BuyTicket
+                    inputName={inputName}
+                    setInputName={setInputName}
+                    cpf={cpf}
+                    setCpf={setCpf}
+                    setSuccessData={setSuccessData}
+                />
+            </Route>
+            <Route path="/success" exact>
+                <Finish
+                    peopleData={peopleData}
+                    setInputName={setInputName}
+                    setCpf={setCpf}
+                    setSuccessData={setSuccessData}
+                />
             </Route>
         </Switch>
     </BrowserRouter>
